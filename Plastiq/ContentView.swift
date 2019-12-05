667ffa544b3ed2267b9b97e748fe81f8569b2134
Nikita <Zhukov>
2019-12-06 00:05:04 +0300

    Initial Commit

---
+++ Plastiq/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plastiq
+//
+//  Created by kokozzz on 6/12/19.
+//  Copyright © 2019 kokozzz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
